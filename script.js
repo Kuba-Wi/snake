238827d1b3c6791e13b98c168358ada488ce0260
kuba <winczukjakub@gmail.com>
2022-11-28 21:02:03 +0100

    added start game button

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ import { Snake, direction } from "./snake.js";
 import { Food } from "./food.js"
 
 let frame_count = 0;
-
 const board_size = 21;
+let board = document.getElementById("board");
+
 let snake = new Snake(board_size);
 let food = new Food(board_size);
 let new_direction = direction.up;
@@ -15,10 +16,30 @@ if (document.readyState == "loading") {
 }
 
 function main() {
-    set_food_position()
-    refresh_board()
+    check_start_pressed()
     listen_arrow_presses()
-    window.requestAnimationFrame(game_loop)
+}
+
+function check_start_pressed() {
+    let start_button = document.getElementsByClassName("start-btn")[0]
+    start_button.addEventListener("click", function(event){
+        event.target.style.visibility = "hidden"
+        set_food_position()
+        refresh_board()
+        window.requestAnimationFrame(game_loop)
+    })
+}
+
+function reset_game() {
+    snake.reset_snake()
+    board.innerHTML = ""
+
+    let start_btn = document.createElement("input")
+    start_btn.classList.add("start-btn")
+    start_btn.value = "Start game"
+    start_btn.type = "button"
+    board.appendChild(start_btn)
+    check_start_pressed()
 }
 
 function game_loop() {
@@ -28,16 +49,17 @@ function game_loop() {
         snake.move()
         if (snake.is_snake_collision()) {
             alert("game over")
-            snake.reset_snake()
-            set_food_position()
+            reset_game()
+            return
         }
-        if (snake.is_snake_position(food.x, food.y)) {
+        else if (snake.is_snake_position(food.x, food.y)) {
             snake.add_body()
             set_food_position()
         }
         refresh_board()
         frame_count = 0
     }
+
     window.requestAnimationFrame(game_loop)
 }
 
@@ -48,7 +70,6 @@ function set_food_position() {
 }
 
 function refresh_board() {
-    let board = document.getElementById("board")
     board.innerHTML = ""
     snake.draw()
     food.draw()
